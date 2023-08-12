5b4fc1f15b906947e9f29b8803fe44e425c787e7
Zakawat Abbas <zakawatabbas123@gmail.com>
2023-08-12 21:27:08 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
+# from flask import Flask, request
+
+# app = Flask(__name__)
+
+
+# # @app.route('/predict', methods=['POST'])
+# # def predict():
+# #     img = request.files['file']
+# #     filename = 'input.jpg'
+# #     # img.save(filename)
+
+# #     # result = predict_image(filename)
+
+# #     result = 'H'
+
+# #     return result
+# @app.route('/')
+# def hello():
+#     return 'Hello, World!'
+
+
+# app.run(host="0.0.0.0", port=5000)
+
 from flask import Flask, request
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello, World!"
 
 
 @app.route('/predict', methods=['POST'])
@@ -16,4 +44,5 @@ def predict():
     return result
 
 
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(debug=True)
